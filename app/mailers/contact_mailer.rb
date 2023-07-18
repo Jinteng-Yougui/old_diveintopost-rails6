852696df4823b80addbd8bf5aa700a98c6bfa525
yuki kondo <jinteng.yougui@gmail.com>
2023-07-18 13:43:10 +0900

    🍌 i18n

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ApplicationMailer
-  def contact_mail(agenda)
-    @agenda = agenda
-    @users = Team.find(@agenda.team_id).users
-    mail to: @users.pluck(:email), subject: I18n.t('views.messages.delete_agenda_notice')
-  end
-end
